6116e8a3e6b2e48d35ff82726bf94e60626f560a
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2023-10-24 15:57:49 +0700

    add qr code display

--- react-p5-demo/src/pages/InputMelody.js
+++ react-p5-demo/src/pages/InputMelody.js
@@ -1,5 +1,6 @@
 import React from "react";
 import io from 'socket.io-client';
+import QRCode from 'qrcode.react'; // Import the QRCode component
 
 class InputMelody extends React.Component {
   constructor(props) {
@@ -8,6 +9,9 @@ class InputMelody extends React.Component {
     this.socket = null;
     this.state = {
       data: null,
+      message: "Start recording",
+      showPopup: false,
+      qrCodeData: "https://github.com/antonioerdeljac/next13-spotify", // Replace with your QR code data
     };
     this.fetchInterval = null;
   }
@@ -21,12 +25,15 @@ class InputMelody extends React.Component {
   }
 
   startDataFetching = () => {
+    this.setState({
+      message: "Recording"
+    });
     this.datas = [];
     this.socket = io('http://localhost:8888');
     this.fetchInterval = setInterval(() => {
       // Start fetching data from the socket here
       this.socket.on('serialdata', (data) => {
-        this.datas.push(data.data)
+        this.datas.push(data.data);
         // Update the data when new data arrives
         this.setState({
           data: data.data,
@@ -38,20 +45,48 @@ class InputMelody extends React.Component {
     setTimeout(() => {
       clearInterval(this.fetchInterval);
       this.socket.close();
-      console.log("Stop fetching : "+this.datas);
+      console.log("Stop fetching: " + this.datas);
+      this.setState({
+        message: "View QR Code", // Change button text
+        showPopup: true, // Show the popup
+      });
     }, 10000);
+  }
+
+  closePopup = () => {
+    this.setState({
+      showPopup: false,
+    });
   }
 
   render() {
     return (
-      <div>
-        <h1>Hello</h1>
-        <button onClick={this.startDataFetching}>Start Data Fetching</button>
+      <div className="halloween-input-melody">
+        <h1 className="halloween-header">Hello, Spooky Halloween!</h1>
+        <button
+          onClick={this.startDataFetching}
+          className="halloween-button"
+          style={{ display: this.state.showPopup ? 'none' : 'block' }}
+        >
+          {this.state.message}
+        </button>
+
+        {this.state.showPopup && (
+          <div className="popup-overlay">
+            <div className="popup">
+              <button onClick={this.closePopup} className="close-button">
+                Close
+              </button>
+              <QRCode value={this.state.qrCodeData} /> {/* Display the QR code */}
+            </div>
+          </div>
+        )}
+
         <div>
           Data: {this.state.data}
         </div>
       </div>
-    )
+    );
   }
 }
 
